f2c107355d8fcd6408fd17f6e6534ca6b4867004
HughesSeanKyle <khughessean@yahoo.com>
2022-06-23 17:30:04 +0200

    Connect to db

--- server/app.js
+++ server/app.js
@@ -1,10 +1,31 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+mongoose = require('mongoose');
+require('dotenv').config();
+
+// assign mongoose promise library and connect to database
+mongoose.Promise = global.Promise;
+
+const connectToMongo = async () => {
+	try {
+		// Connect to the MongoDB cluster
+		await mongoose.connect(
+			process.env.MONGO_DB_URI,
+			{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true },
+			() => console.log(' Mongoose is connected')
+		);
+	} catch (e) {
+		console.log(`Database connection error: ${err.message}`);
+	}
+};
+
+connectToMongo();
 
 app.get('/', (req, res) => {
 	res.send('Hello from the root route');
 });
+
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
 	console.log(`Serving from port ${PORT}.`);
