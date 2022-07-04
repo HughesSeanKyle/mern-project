26871496f209de500f56205ac3ac37ccecd67afa
HughesSeanKyle <khughessean@yahoo.com>
2022-07-04 18:06:25 +0200

    Get authed user profile

--- server/routes/api/profile.js
+++ server/routes/api/profile.js
@@ -1,14 +1,53 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route GET api/profile
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route GET api/profile/
+// // Route reserved to get all profiles
 // @desc Test route
 // @access public (no auth checks yet)
-/*
-    When this route becomes private then a token will need to be sent along to validate if the client can access this route 
-*/
-router.get('/profile', (req, res) => {
-	res.send('Whatup from the profile get route');
+
+// @route GET api/profile/me
+// @desc get the signed in user profile
+// @access pvt route => route accessed via token
+router.get('/profile/me', auth, async (req, res) => {
+	try {
+		// See #Note 1 below
+		const profile = await Profile.findOne({ user: req.user.id }).populate(
+			'user',
+			['name', 'avatar']
+		);
+
+		if (!profile) {
+			return res.status(400).json({ msg: 'There is no profile for this user' });
+		}
+	} catch (err) {
+		console.error(err.message);
+		res.status(500).send('Server Error');
+	}
 });
 
 module.exports = router;
+
+/*
+    #Note 1
+    - const profile = await Profile.findOne({ user: req.user.id }).populate(
+			'user',
+			['name', 'avatar']
+		);
+
+        - Find the profile which matches the req.user.id 
+            - req.user.id 
+                - This is the decrypted JWT user id returned from auth 
+                - If this user is not returned then this route cannot be accessed  
+
+        - .populate(
+			'user',
+			['name', 'avatar']
+		);
+            - Include these fields from the user model when returning the user Profile 
+
+*/
