5fe226f9d577f6b154b0910df374c62cab391a5f
HughesSeanKyle <khughessean@yahoo.com>
2022-07-30 15:23:47 +0200

    Post route implemented - Create post

--- server/routes/api/post.js
+++ server/routes/api/post.js
@@ -1,14 +1,55 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const { check, validationResult } = require('express-validator');
 
-// @route GET api/post
-// @desc Test route
-// @access public (no auth checks yet)
-/*
-    When this route becomes private then a token will need to be sent along to validate if the client can access this route 
-*/
-router.get('/post', (req, res) => {
-	res.send('Whatup from the post route');
-});
+// Models
+const Post = require('../../models/Post');
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+const mongoose = require('mongoose');
+
+// @route POST /post
+// @desc Create a post
+// @access private (USer must be logged in to create a post)
+
+router.post(
+	'/posts',
+	[auth, [check('text', 'Text is required').not().isEmpty()]],
+	async (req, res) => {
+		console.log('Posts route hit');
+
+		const errors = validationResult(req);
+
+		// If errors array is not empty
+		if (!errors.isEmpty()) {
+			// Bad request
+			return res.status(400).json({ errors: errors.array() });
+		}
+
+		try {
+			// The user will be logged in and the userId will be in the token
+			// User object made available via decrypted JWT using auth middleware
+			const user = await User.findById(req.user.id).select('-password');
+
+			// Create new post instance from Post model
+			const newPost = new Post({
+				text: req.body.text,
+				name: user.name,
+				avatar: user.avatar,
+				user: req.user.id,
+			});
+
+			const post = await newPost.save();
+
+			res.json({
+				data: post,
+			});
+		} catch (err) {
+			console.error(err.message);
+			res.status(500).send('Server Error');
+		}
+	}
+);
 
 module.exports = router;
